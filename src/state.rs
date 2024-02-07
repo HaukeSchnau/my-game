7cd41473ee31be58f56f5b77d91d072baf67ecd5
Hauke Schnau <hauke@schnau-lilienthal.de>
2024-02-07 01:09:59 +0100

    Draw entire model in model.rs

--- src/state.rs
+++ src/state.rs
@@ -371,10 +371,7 @@ impl<'window> State<'window> {
             render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
             render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
             render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
-
-            let mesh = &self.obj_model.meshes[0];
-            let material = &self.obj_model.materials[mesh.material];
-            render_pass.draw_mesh_instanced(mesh, material, 0..self.instances.len() as u32, &self.camera_bind_group);
+            render_pass.draw_model_instanced(&self.obj_model, 0..self.instances.len() as u32, &self.camera_bind_group);
         }
 
         // submit will accept anything that implements IntoIter
